0a77f116856d7b22e8ce5bc823af6e3217b8505a
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-01 23:01:24 -0600

    correccion de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer/footer.component';
-import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
