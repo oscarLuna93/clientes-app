4ce25475aea5284d44b6b0535f757651ea7198c0
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-03 10:50:41 -0600

    actualizacion de service para consumir REST

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente.js';
 import { CLIENTES } from './clientes.json';
 import { Observable, of} from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private URL_ENDPOINT: string = "http://localhost:8080/api/clientes"
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.URL_ENDPOINT).pipe(
+      map((response) => response as Cliente[])
+    );
   }
 }
