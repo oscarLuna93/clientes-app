e57df2870e6069f4d5fb7c4f9b4eaaf5b76dc817
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-03 15:25:16 -0600

    formulario para crear cliente

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-form',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  private cliente: Cliente = new Cliente()
+  private titulo: string = "Crear Cliente";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public create(): void {
+    console.log(this.cliente)
   }
 
 }
