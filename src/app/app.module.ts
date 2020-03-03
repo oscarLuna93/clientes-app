6629aea2adb6cb8029627fdb7ee0fb3770f79330
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-02 18:26:38 -0600

    rutas para componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { ClientesComponent } from './clientes/clientes.component';
     ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
