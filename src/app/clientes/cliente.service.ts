2deea7a7ba93d2578e26a4dc395d318fd069d501
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-04 15:19:14 -0600

    funcion en servicio getCliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -23,4 +23,8 @@ export class ClienteService {
   create(cliente: Cliente): Observable<Cliente> {
     return this.http.post<Cliente>(this.URL_ENDPOINT, cliente, {headers: this.httpHeaders});
   }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.URL_ENDPOINT}/${id}`);
+  }
 }
