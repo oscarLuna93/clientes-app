1c597ffad400584625c6b2f852c0f7eb05241494
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-02 16:20:45 -0600

    implementacion eventemitter al subir foto

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -31,6 +31,15 @@ export class ClientesComponent implements OnInit{
       this.paginador = response;
       });
     });
+
+    this.modalService.notificarUpload.subscribe(cliente => {
+      this.clientes = this.clientes.map(clienteOriginal => {
+        if (cliente.id == clienteOriginal.id) {
+          clienteOriginal.foto = cliente.foto;
+        }
+        return clienteOriginal;
+      })
+    })
   }
 
   delete(cliente: Cliente): void {
--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -44,6 +44,9 @@ export class DetalleComponent implements OnInit {
         } else if(event.type === HttpEventType.Response) {
           let response: any = event.body;
           this.cliente = response.cliente as Cliente;
+          
+          this.modalService.notificarUpload.emit(this.cliente);
+          
           Swal.fire('La foto se ha subido correctamente', response.mensaje, 'success');
         }
       });
