29b3aca35bd12009be0ba0724c690c68320fc6d0
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-01 18:04:54 -0600

    validacion si existe imagen y formato

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -31,15 +31,23 @@ export class DetalleComponent implements OnInit {
 
   seleccionarFoto(event) {
     this.fotoSeleccionada = event.target.files[0];
-    console.log(this.fotoSeleccionada); 
+    console.log(this.fotoSeleccionada);
+    if (this.fotoSeleccionada.type.indexOf('image')) {
+      Swal.fire('Error: ', 'Debes seleccionar un archivo de tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
   subirFoto() {
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
-    .subscribe(cliente => {
-      this.cliente = cliente;
-      Swal.fire('La foto se ha subido correctamente', `La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
-    });
+    if (!this.fotoSeleccionada) {
+      Swal.fire('Error: ', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
+      .subscribe(cliente => {
+        this.cliente = cliente;
+        Swal.fire('La foto se ha subido correctamente', `La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
+      });
+    }
   }
 
 }
