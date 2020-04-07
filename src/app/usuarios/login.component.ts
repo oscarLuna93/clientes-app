c4ee96f8721bf3fadc5e27981e4d3c82b7ae08e7
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-07 17:27:39 -0500

    metodo login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = 'inicia sesion por favor';
-
-  constructor() { }
+  usuario: Usuario;
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
   }
 
+  login():void {
+    console.log(this.usuario);
+    if ((this.usuario.username == null || this.usuario.password == null)
+        || (this.usuario.username == '' || this.usuario.password)) {
+      Swal.fire('Error login', 'Usuario o contraseña vacias!', 'error');
+      return;
+    }
+  }
 }
