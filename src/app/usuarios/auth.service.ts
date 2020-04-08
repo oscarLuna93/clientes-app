d9cbad76b4371a90e0e79c1a07672f303703783e
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-07 23:13:10 -0500

    service para autenticacion

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario:Usuario):Observable<any> {
+    const urlEndPoint = 'http://localhost:8080/oauth/token';
+    const credencialess = btoa('angularapp' + ':' + '12345');
+    const httpHeaders = new HttpHeaders({'Content-Type:': 'application/x-www-form-urlencoded'
+                            ,'authorization': 'Basic ' + credencialess});
+    
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+
+    return this.http.post<any>(urlEndPoint, params, {headers: httpHeaders});
+  }
+}
