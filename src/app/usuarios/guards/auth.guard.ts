6c11e1745c6bcd4a6bb1599e026e20037810bf53
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-08 17:35:57 -0500

    validacion cuando expira token

--- src/app/usuarios/guards/auth.guard.ts
+++ src/app/usuarios/guards/auth.guard.ts
@@ -14,9 +14,25 @@ export class AuthGuard implements  CanActivate{
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       if(this.authService.isAuthenticated()){
+        if (this.isTokenExpired) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+          return false;
+        }
         return true;
       }
       this.router.navigate(['/login']);
+      return false;
+    }
+
+    isTokenExpired(): boolean {
+      let token = this.authService.token;
+      let payload = this.authService.obtenerDatosToken(token);
+      let now = new Date().getTime() / 1000;
+
+      if (payload.exp < now) {
+        return true;
+      }
       return false;
     }
 }
