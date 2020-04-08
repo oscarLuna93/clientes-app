d356f6df3b673176dbe112e1b5aa35667af2386c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-08 15:27:49 -0500

    boton logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../usuarios/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title:string = 'App angular';
+
+  constructor(private authService: AuthService
+              ,private router: Router) {
+  }
+
+  logout():void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
