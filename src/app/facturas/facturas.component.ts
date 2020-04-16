9d962a64694507ffa360811052edde14eb631621
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-16 12:01:07 -0500

    generando linea de factura

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
 import { map, flatMap } from 'rxjs/operators';
 import { FacturaService } from './services/facturas.service';
 import { Producto } from './models/producto';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
+import { ItemFactura } from './models/item-factura';
 
 @Component({
   selector: 'app-facturas',
@@ -47,5 +49,17 @@ export class FacturasComponent implements OnInit {
 
   mostrarNombre(producto?: Producto): string | undefined {
     return producto? producto.nombre: undefined;
+  }
+  
+  seleccionarProducto(event: MatAutocompleteSelectedEvent) {
+    let producto = event.option.value as Producto;
+    
+    let nuevoItem = new ItemFactura();
+    nuevoItem.producto = producto;
+    this.factura.items.push(nuevoItem);
+    
+    this.autoCompleteControl.setValue('');
+    event.option.focus();
+    event.option.deselect();
   }
 }
